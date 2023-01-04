c2972c2d94b8b7fc853f2e99e05dfd0228b93c20
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-04 19:36:27 +0100

    terminado

--- ex3.js
+++ ex3.js
@@ -45,7 +45,7 @@ function sumAll(arrayOfNumbers){
     for (i = 0; i < arrayOfNumbers.length; i++){
     sumTotal += arrayOfNumbers[i];
 }
-return console.log(sum);
+return console.log(sumTotal);
 }
 
 sumAll(numbers);
@@ -68,7 +68,50 @@ average(numbers1);
 
 //!Interacción 5
 
-//? Calcular el promedio de strings. 
+//? Calcular el promedio de strings. Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+
+function averageWord(param){
+let sum1 = 0;
+for (i = 0; i < param.length; i++){
+    if (typeof param[i] === 'string'){
+        sum += param[i].length
+    }
+}
+return console.log(sum1 / param.length)
+}
+
+averageWord(mixedElements)
+
+
+//!Interacción 6
+
+//? Valores únicos, recorrer el array, eleminar elementos duplicados y retornar array sin elementos duplicados. 
+
+const duplicates = ['sushi',
+'pizza',
+'burger',
+'potatoe',
+'pasta',
+'ice-cream',
+'pizza',
+'chicken',
+'onion rings',
+'pasta',
+'soda']; 
+
+function removeDuplicates(arrayOfObjects){
+    let uniquesObjects = '';
+    for (i = 0; i < arrayOfObjects.length; i++){
+        if (arrayOfObjects[i] === duplicates);{
+        arrayOfObjects.splice(uniquesObjects, 1)
+    }
+}
+return arrayOfObjects
+}
+
+console.log(removeDuplicates(duplicates));
 
 
 
