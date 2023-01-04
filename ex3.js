9b736c1bc05f2703b7066cd807b1fb755ee1456a
javieralvarez <javier.alvarez.gonzalez@live.com>
2023-01-04 17:34:42 +0100

    retomamos

--- ex3.js
+++ ex3.js
@@ -1,3 +1,7 @@
+//!Interacción 1
+
+//?
+
 function sum(numberOne, numberTwo) {
     if (numberOne > numberTwo){
         return numberOne
@@ -6,7 +10,35 @@ function sum(numberOne, numberTwo) {
     }
 }
 
+sum(3,1)
 
-sum(2,1);
+//Falta por ejecutar
+
+//!Interacción 2
+
+
+//?Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tengan la misma longitud deberá devolver el primero
+
+const avengers = [
+    "Hulk",
+    "Thor",
+    "IronMan",
+    "Captain A.",
+    "Spiderman",
+    "Captain M.",
+  ];
+  
+
+  console.log(avengers)
+
+  //!Interacción 3
+
+  //? Implementa una función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz
+
+  let listaCompra = ['zanahorias', 'queso', 'pasta', 'calabacines', 'raviolis', 'pizza'];
+
+
+
+
 
 
